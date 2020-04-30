4e6e2893292fc96f59f42bd0990d7cdd63f3bf90
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-30 12:07:38 -0500

    created classes for all 4 types needed

---
+++ main.py
@@ -0,0 +1,11 @@
+from cohort import Cohort
+from exercise import Exercise
+from instructor import Instructor
+from student import Student
+
+exercise_one = Exercise("Exercise One", "Python")
+exercise_two = Exercise("Exercise Two", "Python")
+exercise_three = Exercise("Exercise Three", "C#")
+exercise_four = Exercise("Exercise Four", "C#")
+
+cohort_one = Cohort()
