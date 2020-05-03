bfda09cbfd10002ded362b2bcac83af43c2bb88b
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-03 16:42:47 -0500

    steps 1 and 2 complete

--- reports.py
+++ reports.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 class Student():
-
     def __init__(self, first, last, handle, cohort):
         self.first_name = first
         self.last_name = last
@@ -15,8 +14,16 @@ class Cohort():
     def __init__(self, name):
         self.cohortName = name
 
-    # def __repr__(self):
-    #     return f'{self.first_name} {self.last_name} is in {self.cohort}'
+    def __repr__(self):
+        return f'{self.cohortName}'
+
+class Exercise():
+    def __init__(self, name, language):
+        self.exerciseName = name
+        self.codeLanguage = language
+
+    def __repr__(self):
+        return f'Exercise: {self.exerciseName} Language used: {self.codeLanguage}'
 
 class StudentExerciseReports():
 
@@ -61,5 +68,60 @@ class StudentExerciseReports():
             # Same as above but in list comprehension format
             # [print(s) for s in all_students]
 
+    def all_cohorts(self):
+
+        """Retrieve all cohorts and display cohort name"""
+
+        # Making a connection with sqlite disk server with the path to my .db file
+        with sqlite3.connect(self.db_path) as conn:
+            # Making a cohort obj instance for each row in Cohort table
+            conn.row_factory = lambda cursor, row: Cohort(
+                row[1]
+            )
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            SELECT cohort.id, cohort.cohortName
+            FROM cohort
+            ORDER BY cohort.id
+            """)
+
+            all_cohorts = db_cursor.fetchall()
+
+            for cohort in all_cohorts:
+                print(cohort)
+
+            # Same as above but in list comprehension format
+            # [print(s) for s in all_students]
+
+    def all_exercises(self):
+
+        """Retrieve all exercises with exercise name and 
+        code language used"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = lambda cursor, row: Exercise(
+                row[1], row[2]
+            )
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            SELECT exercise.id,
+                exercise.exerciseName,
+                exercise.codeLanguage
+            FROM exercise
+            ORDER BY exercise.id
+            """)
+
+            all_exercises = db_cursor.fetchall()
+
+            for exercise in all_exercises:
+                print(exercise)
+
+            # Same as above but in list comprehension format
+            # [print(s) for s in all_students]
+
 reports = StudentExerciseReports()
-reports.all_students()
+# reports.all_students()
+# reports.all_cohorts()
+# reports.all_exercises()
